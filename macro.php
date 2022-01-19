801a8c4d912a75090c782d5a520915b09c2dfa64
Aaron Francis <aarondfrancis@gmail.com>
2022-01-19 14:27:40 -0600

    Update macro to work with relations. See conversation at PR #1

--- macro.php
+++ macro.php
@@ -11,30 +11,44 @@ Builder::macro('deferredPaginate', function ($perPage = null, $columns = ['*'], 
         ->setEagerLoads([])
         // Only select the primary key, we'll get the full
         // records in a second query below.
-        ->select("{$table}.{$key}")
-        // Pass through the params to the real paginator.
-        ->paginate($perPage, $columns, $pageName, $page);
+        ->paginate($perPage, ["$table.$key"], $pageName, $page);
 
-    // Add our values in directly using "raw" instead of adding new bindings.
+    // Add our values in directly using "raw," instead of adding new bindings.
     // This is basically the `whereIntegerInRaw` that Laravel uses in some
     // places, but we're not guaranteed the primary keys are integers, so
     // we can't use that. We're sure that these values are safe because
     // they came directly from the database in the first place.
     $this->query->wheres[] = [
         'type'   => 'InRaw',
-        'column' => "{$table}.{$key}",
+        'column' => "$table.$key",
         // Get the key values from the records on the *current* page, without mutating them.
         'values'  => $paginator->getCollection()->map->getRawOriginal($key)->toArray(),
-        'boolean' => 'and',
+        'boolean' => 'and'
     ];
+
+    // simplePaginate increments by one to see if there's another page. We'll
+    // decrement to counteract that since it's unnecessary in our situation.
+    $page = $this->simplePaginate($paginator->perPage() - 1, $columns, null, 1);
 
     // Create a new paginator so that we can put our full records in,
     // not the ones that we modified to select only the primary key.
     return new LengthAwarePaginator(
-        $this->get(),
+        $page->items(),
         $paginator->total(),
         $paginator->perPage(),
         $paginator->currentPage(),
         $paginator->getOptions()
     );
+});
+
+Relation::macro('deferredPaginate', function ($perPage = null, $columns = ['*'], $pageName = 'page', $page = null) {
+    if ($this instanceof HasManyThrough || $this instanceof BelongsToMany) {
+        $this->query->addSelect($this->shouldSelect($columns));
+    }
+
+    return tap($this->query->deferredPaginate($perPage, $columns, $pageName, $page), function ($paginator) {
+        if ($this instanceof BelongsToMany) {
+            $this->hydratePivotRelation($paginator->items());
+        }
+    });
 });
